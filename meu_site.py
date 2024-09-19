94a7c92387a94ebebeb3fb39d7bdef026ed98a98
Luan <luanferreiralima280@gmail.com>
2024-09-19 11:35:14 -0300

    Initial commit

---
+++ meu_site.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#Função
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+
+# coloca o site no ar
+if __name__ == "__main__":
+    app.run(debug=True)
